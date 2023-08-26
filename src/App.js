9c60d27a9f21c79ed4471941f3fc3f0991225fb6
Cristian Villa <c.villavirgen@gmail.com>
2023-08-26 01:48:50 -0600

    Refactor components to meet project requirements

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BooksList from './components/BooksList';
 import BookForm from './components/BookForm';
 import Navigation from './components/Navigation';
-import Categories from './components/Categories';  // Add this line
+import Categories from './components/Categories'; // Add this line
 
 const HomePage = () => (
   <div>
@@ -18,7 +18,8 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/categories" element={<Categories />} />  // Updated this line
+        <Route path="/categories" element={<Categories />} />
+        {' '}
       </Routes>
     </Router>
   );
