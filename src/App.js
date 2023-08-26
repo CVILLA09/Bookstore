dd484ac70b0c92ec84fcbc5e454e46e6c6e1b591
Cristian Villa <c.villavirgen@gmail.com>
2023-08-25 20:13:57 -0600

    Integrate components into App and set up basic structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import CategoriesPage from './components/CategoriesPage';
-import logo from './logo.svg';
-import './App.css';
+import BooksList from './components/BooksList';
+import BookForm from './components/BookForm';
+import Navigation from './components/Navigation';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <BooksList />
+      <BookForm />
+    </div>
+  );
+};
+
+const CategoriesPage = () => {
+  return <h1>Categories Page</h1>;
+};
 
 function App() {
   return (
     <Router>
+      <Navigation />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/categories" element={<CategoriesPage />} />
