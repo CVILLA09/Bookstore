754d959db4b84f30ef414a56c1ac8be4ac725655
Cristian Villa <c.villavirgen@gmail.com>
2023-08-26 02:31:55 -0600

    Erase repeated component BooksList and updated imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import BooksList from './components/BooksList';
+import BookList from './components/BookList';
 import NewBookForm from './components/NewBookForm';
 import Navigation from './components/Navigation';
 import Categories from './components/Categories';
@@ -7,7 +7,7 @@ import Categories from './components/Categories';
 const HomePage = () => (
   <div>
     <h1>Home Page</h1>
-    <BooksList />
+    <BookList />
     <NewBookForm />
   </div>
 );
