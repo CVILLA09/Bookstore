30f0a26ec19d1c74512178d5403c3a867941f96c
Cristian Villa <c.villavirgen@gmail.com>
2023-08-30 20:47:31 -0600

    Update BookList.js to handle API response and fix prop types

--- src/api/bookstoreAPI.js
+++ src/api/bookstoreAPI.js
@@ -22,7 +22,7 @@ export const addBookAPI = async (appId, book) => {
     const response = await api.post(url, book);
     return response.data;
   } catch (error) {
-    //console.error('Error adding book:', error);
+    // console.error('Error adding book:', error);
     return null;
   }
 };
@@ -34,7 +34,7 @@ export const removeBookAPI = async (appId, itemId) => {
     const response = await api.delete(url);
     return response.data;
   } catch (error) {
-    //console.error('Error removing book:', error);
+    // console.error('Error removing book:', error);
     return null;
   }
 };
