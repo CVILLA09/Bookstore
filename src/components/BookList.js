b92927065ba7abc8e10d68e8fa4d43e62df3b35e
Cristian Villa <c.villavirgen@gmail.com>
2023-08-30 19:52:01 -0600

    Incorporate app ID creation and book fetching logic in BookList component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,32 +1,35 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import api from '../api/bookstoreAPI'; // Import the Axios instance
+import api, { createApp } from '../api/bookstoreAPI'; // Import the Axios instance and createApp function
 import Book from './Book';
-import { addBooksToStore } from '../redux/books/booksSlice'; // Import the new action creator
+import { addBooksToStore } from '../redux/books/booksSlice';
 
 const BookList = () => {
-  // Use useSelector to get books from Redux store
   const books = useSelector((state) => state.books);
-  
-  // Use useDispatch to dispatch actions to the Redux store
   const dispatch = useDispatch();
 
-  // Use useEffect to fetch books from the API when the component mounts
   useEffect(() => {
-    api.get('/apps/:app_id/books')
-      .then((response) => {
-        const fetchedBooks = Object.values(response.data); // Assuming the data is an object with book IDs as keys
-        dispatch(addBooksToStore(fetchedBooks)); // Dispatch the new action to add books to the Redux store
-      })
-      .catch((error) => {
-        console.error('There was an error fetching books:', error);
-      });
+    const fetchBooks = async () => {
+      const appId = await createApp();
+      if (appId) {
+        const url = `/apps/${appId}/books`;
+        api.get(url)
+          .then((response) => {
+            const fetchedBooks = Object.values(response.data);
+            dispatch(addBooksToStore(fetchedBooks));
+          })
+          .catch((error) => {
+            console.error('There was an error fetching books:', error);
+          });
+      }
+    };
+    fetchBooks();
   }, [dispatch]);
 
   return (
     <div>
       {books.map((book) => (
-        <Book key={book.item_id} book={book} /> // Changed 'key' to use 'item_id'
+        <Book key={book.item_id} book={book} />
       ))}
     </div>
   );
