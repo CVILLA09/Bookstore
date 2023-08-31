c6f84c84d198d4446ebf91c56f113fa96e555e32
Cristian Villa <c.villavirgen@gmail.com>
2023-08-30 19:49:58 -0600

    Add function to create new app and get its ID in bookstoreAPI.js

--- src/api/bookstoreAPI.js
+++ src/api/bookstoreAPI.js
@@ -4,4 +4,15 @@ const api = axios.create({
   baseURL: 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi',
 });
 
+// Function to create a new app and get its ID
+export const createApp = async () => {
+  try {
+    const response = await api.post('/apps/');
+    return response.data;
+  } catch (error) {
+    console.error("Error creating app:", error);
+    return null;
+  }
+};
+
 export default api;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import api from '../api/bookstoreAPI'; // Import the Axios instance
 import Book from './Book';
+import { addBooksToStore } from '../redux/books/booksSlice'; // Import the new action creator
 
 const BookList = () => {
   // Use useSelector to get books from Redux store
   const books = useSelector((state) => state.books);
+  
+  // Use useDispatch to dispatch actions to the Redux store
+  const dispatch = useDispatch();
+
+  // Use useEffect to fetch books from the API when the component mounts
+  useEffect(() => {
+    api.get('/apps/:app_id/books')
+      .then((response) => {
+        const fetchedBooks = Object.values(response.data); // Assuming the data is an object with book IDs as keys
+        dispatch(addBooksToStore(fetchedBooks)); // Dispatch the new action to add books to the Redux store
+      })
+      .catch((error) => {
+        console.error('There was an error fetching books:', error);
+      });
+  }, [dispatch]);
 
   return (
     <div>
       {books.map((book) => (
-        <Book key={book.id} book={book} />
+        <Book key={book.item_id} book={book} /> // Changed 'key' to use 'item_id'
       ))}
     </div>
   );
