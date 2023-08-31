2f6a62f6fee1b846a682b82c2114541b89872f6a
Cristian Villa <c.villavirgen@gmail.com>
2023-08-30 21:25:24 -0600

    Integrated App ID from .env into booksSlice.js for API calls.

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,7 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { createApp, addBookAPI, removeBookAPI } from '../../api/bookstoreAPI'; // import the new API functions
+import { addBookAPI, removeBookAPI } from '../../api/bookstoreAPI';
 
 const initialState = [];
+
+// Fetch App ID from .env file
+const appId = process.env.REACT_APP_BOOKSTORE_APP_ID;
 
 export const booksSlice = createSlice({
   name: 'books',
@@ -24,23 +27,17 @@ export const { addBooksToStore, addBook, removeBook } = booksSlice.actions;
 
 // Create an async thunk for adding a book
 export const addBookAsync = (book) => async (dispatch) => {
-  const appId = await createApp();
-  if (appId) {
-    const addedBook = await addBookAPI(appId, book);
-    if (addedBook) {
-      dispatch(addBook(addedBook));
-    }
+  const addedBook = await addBookAPI(appId, book);
+  if (addedBook) {
+    dispatch(addBook(addedBook));
   }
 };
 
 // Create an async thunk for removing a book
 export const removeBookAsync = (itemId) => async (dispatch) => {
-  const appId = await createApp();
-  if (appId) {
-    const removed = await removeBookAPI(appId, itemId);
-    if (removed) {
-      dispatch(removeBook(itemId));
-    }
+  const removed = await removeBookAPI(appId, itemId);
+  if (removed) {
+    dispatch(removeBook(itemId));
   }
 };
 
