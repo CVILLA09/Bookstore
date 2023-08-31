71a9215039ec60287d5d9d63d2cee059391a8a7d
Cristian Villa <c.villavirgen@gmail.com>
2023-08-30 19:43:08 -0600

    Add action to update initial state with books fetched from API in booksSlice

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,30 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = [
-  {
-    item_id: 'item1',
-    title: 'The Great Gatsby',
-    author: 'John Smith',
-    category: 'Fiction',
-  },
-  {
-    item_id: 'item2',
-    title: 'Anna Karenina',
-    author: 'Leo Tolstoy',
-    category: 'Fiction',
-  },
-  {
-    item_id: 'item3',
-    title: 'The Selfish Gene',
-    author: 'Richard Dawkins',
-    category: 'Nonfiction',
-  },
-];
+const initialState = [];
 
 export const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
+    addBooksToStore: (state, action) => {
+      return action.payload;
+    },
     addBook: (state, action) => {
       state.push(action.payload);
     },
@@ -37,5 +21,5 @@ export const booksSlice = createSlice({
   },
 });
 
-export const { addBook, removeBook } = booksSlice.actions;
+export const { addBooksToStore, addBook, removeBook } = booksSlice.actions;
 export default booksSlice.reducer;
