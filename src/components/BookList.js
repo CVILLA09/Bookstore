7c1850f049e03160c8cbe52cd65db17d564bad8f
Cristian Villa <c.villavirgen@gmail.com>
2023-08-30 21:28:48 -0600

    Refactor BookList.js to use App ID from .env for API calls.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import api, { createApp } from '../api/bookstoreAPI';
+import api from '../api/bookstoreAPI';
 import Book from './Book';
 import { addBooksToStore } from '../redux/books/booksSlice';
 
@@ -8,20 +8,20 @@ const BookList = () => {
   const books = useSelector((state) => state.books);
   const dispatch = useDispatch();
 
+  // Fetch App ID from .env file
+  const appId = process.env.REACT_APP_BOOKSTORE_APP_ID;
+
   useEffect(() => {
     const fetchBooks = async () => {
-      const appId = await createApp();
-      if (appId) {
-        const url = `/apps/${appId}/books`;
-        api.get(url)
-          .then((response) => {
-            const fetchedBooks = Object.values(response.data).flat();
-            dispatch(addBooksToStore(fetchedBooks));
-          })
-          .catch(() => {
-            // Handle error
-          });
-      }
+      const url = `/apps/${appId}/books`;
+      api.get(url)
+        .then((response) => {
+          const fetchedBooks = Object.values(response.data).flat();
+          dispatch(addBooksToStore(fetchedBooks));
+        })
+        .catch(() => {
+          // Handle error
+        });
     };
     fetchBooks();
   }, [dispatch]);
