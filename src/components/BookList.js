c2ab2b0dc3a5df4e2aa13f73011e48c276b6f3dc
Cristian Villa <c.villavirgen@gmail.com>
2023-09-01 17:01:36 -0600

    Integrated async actions and updated state in BookList component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Book from './Book';
+import { fetchBooksAsync, removeBookAsync } from '../redux/books/booksSlice';
 
 const BookList = () => {
-  // Use useSelector to get books from Redux store
-  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  // Fetch books from the API when the component mounts
+  useEffect(() => {
+    dispatch(fetchBooksAsync());
+  }, [dispatch]);
+
+  // Use useSelector to get books and status from Redux store
+  const books = useSelector((state) => state.books.books);
+  const status = useSelector((state) => state.books.status);
+
+  // Function to handle book removal
+  const handleRemoveBook = (id) => {
+    dispatch(removeBookAsync(id));
+  };
+
+  // Loading and error states
+  if (status === 'loading') return <p>Loading...</p>;
+  if (status === 'failed') return <p>Error loading books</p>;
 
   return (
     <div>
       {books.map((book) => (
-        <Book key={book.id} book={book} />
+        <Book key={book.id} book={book} onRemove={handleRemoveBook} />
       ))}
     </div>
   );
